95f98de00077b1fb878b82e1d7ee522e631901c8
goeringkenneth <71526592+goeringkenneth@users.noreply.github.com>
2023-03-01 08:24:26 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger=document.querySelector('.hamburger')
+const navLinks=document.querySelector('.nav-links')
+const line=document.querySelector('.line')
+
+
+const links=document.querySelectorAll('.nav-links li')
+
+hamburger.addEventListener("click", ()=> {
+    line.classList.toggle("open");
+    navLinks.classList.toggle("open");
+    links.forEach(link =>{
+        link.classList.toggle("fade")
+    });
+});
